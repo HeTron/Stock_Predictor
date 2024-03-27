c39e1f6d4d072bf52a77a6b6f332333c02bd5072
HeTron <jcode2k@gmail.com>
2024-03-27 18:08:12 -0400

    added a visualization next to the price predictions

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from trading_predictor import fetch_data, get_start_date, preprocess_data, training_data_prep, model_operation
 from dotenv import load_dotenv
 import os
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 load_dotenv()
 token = os.getenv('TIINGO_API_KEY')
@@ -27,7 +29,35 @@ if st.button('Predict'):
 
         predictions_df = model_operation(X_train, y_train, stock_data)
 
-        st.write(predictions_df)
+        # Create a figure and axis for the plot
+        fig, ax = plt.subplots()
+
+        # Plot the predicted adjusted close prices
+        ax.plot(predictions_df.index, predictions_df['Predicted Adj Close'], label='Predicted Price')
+
+        # Rotate date labels and set format
+        plt.xticks(rotation=45)
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=2))
+
+        # Optionally, add a title and labels
+        ax.set_title('Stock Price Prediction')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Price Prediction')
+
+        # Display legend
+        ax.legend()
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.write("Results:")
+            st.dataframe(predictions_df)
+
+        with col2:
+            st.write("Visualization:")
+            st.pyplot(fig)
+
     else:
         st.write("Please enter a valid stock symbol.")
 
