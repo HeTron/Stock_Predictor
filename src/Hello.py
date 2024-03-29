f95ea205469bce4a6214862e29afa8ec5c4b0803
Bryan Munera <bryanmunera@hotmail.com>
2024-03-29 17:30:38 -0400

    image update

--- src/Hello.py
+++ src/Hello.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title="Hello",
@@ -12,15 +13,27 @@ st.sidebar.success("Here you can find various tools to help you with your stock 
 st.markdown(
     """
     StockProphet is a web app that allows you to analyze stock data and predict future stock prices.
-    **👈 Select a tab from the sidebar** To begin your stock analysis and find out what StockProphet can do.
+    
+    **👈 Select a tab from the sidebar** to begin your stock analysis and find out what StockProphet can do.
+    
     ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
+    """
 )
+
+# Open the image file
+image = Image.open(r'C:\Users\bryan\PythonWorkSpaces\StockAdvisorFinal\Stock_Predictor\assets\Logo.jpg')
+
+# Get the original width and height of the image
+original_width, original_height = image.size
+
+# Calculate the aspect ratio
+aspect_ratio = original_height / original_width
+
+# Set the desired width
+desired_width = 400
+
+# Calculate the corresponding height based on the aspect ratio
+desired_height = int(desired_width * aspect_ratio)
+
+# Display the image with the desired width and height at the bottom of the page
+st.image(image, caption='Company Logo', width=desired_width, output_format='PNG')
